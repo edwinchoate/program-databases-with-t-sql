0a9b6b1832753a688dc11a59fa2332ff0e7d06db
Edwin Choate <edwin.choate@pm.me>
2024-12-29 12:14:19 -0500

    completes ch. 3 code challenge

--- Ex_Files_Transact_SQL/Exercise Files/Chapter 3/03_06_Challenge-Start.sql
+++ Ex_Files_Transact_SQL/Exercise Files/Chapter 3/03_06_Challenge-Start.sql
@@ -19,3 +19,22 @@ GO
 -- 1) view information from Sales.Customers
 -- 2) view information from Sales.Orders
 -- 3) write a row to Sales.CustomerAccountAudit to log activity
+
+CREATE OR ALTER PROC Sales.uspViewSalesInfoByCustomerID (@CustomerID INT)
+AS
+	SELECT * FROM Sales.Orders
+	WHERE Orders.CustomerID = @CustomerID;
+
+	SELECT * FROM Sales.Customers
+	WHERE Customers.CustomerID = @CustomerID;
+
+	INSERT INTO Sales.CustomerAccountAudit (CustomerID, ReviewDate)
+	VALUES (@CustomerID, GETDATE());
+;
+GO
+
+EXEC Sales.uspViewSalesInfoByCustomerID '7';
+GO
+
+SELECT * FROM Sales.CustomerAccountAudit;
+GO
