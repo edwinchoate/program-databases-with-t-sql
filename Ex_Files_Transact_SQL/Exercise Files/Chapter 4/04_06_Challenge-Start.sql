44add77de44bbad2698623cbdbd508875b823e55
Edwin Choate <edwin.choate@pm.me>
2024-12-29 16:20:53 -0500

    completes ch. 4 code challenge

--- Ex_Files_Transact_SQL/Exercise Files/Chapter 4/04_06_Challenge-Start.sql
+++ Ex_Files_Transact_SQL/Exercise Files/Chapter 4/04_06_Challenge-Start.sql
@@ -13,3 +13,26 @@ GO
 
 -- Create a stored procedure that contains a transaction for transferring funds
 -- Then use the stored procedure to transfer 50.00 from Account 1 to Account 3.
+
+CREATE OR ALTER PROC dbo.TransferFunds (@FromAccountID INT, @ToAccountID INT, @Amount decimal(10,2))
+AS
+SET XACT_ABORT ON;
+BEGIN TRANSACTION;
+	UPDATE dbo.BankAccounts 
+		SET Balance -= @Amount 
+		WHERE AccountID = @FromAccountID;
+	UPDATE dbo.BankAccounts 
+		SET Balance += @Amount 
+		WHERE AccountID = @ToAccountID;
+COMMIT TRANSACTION;
+SET XACT_ABORT OFF;
+;
+GO
+
+EXEC dbo.TransferFunds 1, 3, 50.00;
+GO
+
+-- EXEC dbo.TransferFunds 3, 1, 50.00;
+
+SELECT * FROM dbo.BankAccounts;
+GO
